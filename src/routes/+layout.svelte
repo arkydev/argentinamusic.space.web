894db0824d54482e01aaae9ebff6fd58a3dee5b5
arcbjorn <arky.happ@gmail.com>
2025-09-25 11:28:12 -0300

    fix: improve favicon support for Firefox compatibility

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 
 <svelte:head>
 	<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="/favicon.png" type="image/png" />
 	<link rel="apple-touch-icon" href="/favicon.png" />
 </svelte:head>
 
